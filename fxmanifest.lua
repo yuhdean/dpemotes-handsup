18c5eef63695526cafdb824b69d1e9257fcc67be
Dean <deannobusiness@gmail.com>
2026-03-01 19:33:47 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,19 @@ fx_version 'adamant'
 
 game 'gta5'
 
+shared_script '@ox_lib/init.lua'
+
 client_scripts {
-	'NativeUI.lua',
-	'Config.lua',
-	'Client/*.lua'
+		'NativeUI.lua',
+		'Config.lua',
+		'Client/*.lua'
 }
 
 server_scripts {
-	'Config.lua',
-	'@mysql-async/lib/MySQL.lua',
-	'Server/*.lua'
+		'Config.lua',
+		'@mysql-async/lib/MySQL.lua',
+		'Server/*.lua'
 }
+
+dependency 'ox_lib'
+dependency 'ox_target'
